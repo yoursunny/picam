37df1e52ebbd7a4ff17da3bf9b74287e429d2e11
Junxiao Shi <git@mail1.yoursunny.com>
2018-03-10 19:40:29 -0500

    httpcam: contour.mjpeg

--- httpcam.py
+++ httpcam.py
@@ -32,6 +32,8 @@ class Handler(BaseHTTPRequestHandler, MjpegMixin):
             self.handleCamJpg()
         elif self.path == '/cam.mjpeg':
             self.handleCamMjpeg()
+        elif self.path == '/contour.mjpeg':
+            self.handleContourMjpeg()
         else:
             self.send_response(404)
             self.end_headers()
@@ -48,15 +50,35 @@ class Handler(BaseHTTPRequestHandler, MjpegMixin):
         self.end_headers()
         with PiCamera() as camera:
             camera.resolution = (800, 600)
+            time.sleep(1)
             camera.capture(self.wfile, format='jpeg')
 
     def handleCamMjpeg(self):
         self.mjpegBegin()
         with PiCamera() as camera:
             camera.resolution = (640, 480)
-            time.sleep(1)
-            for dummy in camera.capture_continuous(self.wfile, format='jpeg',
-                                                   use_video_port=True, quality=50):
+            for x in camera.capture_continuous(self.wfile, format='jpeg',
+                                               use_video_port=True, quality=50):
+                self.mjpegEndFrame()
+
+    def handleContourMjpeg(self):
+        width, height, blur, sigma = 640, 480, 2, 0.33
+        import cv2
+        import numpy as np
+        self.mjpegBegin()
+        with PiCamera() as camera:
+            camera.resolution = (width, height)
+            camera.image_denoise = False
+            camera.image_effect = 'blur'
+            camera.image_effect_params = (blur,)
+            yuv = np.empty((int(width * height * 1.5),), dtype=np.uint8)
+            for x in camera.capture_continuous(yuv, format='yuv', use_video_port=False):
+                image = yuv[:width*height].reshape((height, width))
+                v = np.median(image)
+                lower = int(max(0, (1.0 - sigma) * v))
+                upper = int(min(255, (1.0 + sigma) * v))
+                edged = cv2.Canny(image, lower, upper)
+                self.wfile.write(cv2.imencode('.jpg', edged)[1])
                 self.mjpegEndFrame()
 
 
